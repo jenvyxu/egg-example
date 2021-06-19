f32abcb251507ae08dbf07d0b3b2fea8eef91f19
xuzhenwei <1272117264@qq.com>
2021-06-19 16:45:35 +0800

    feat: 实现new的controller和service

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+exports.keys = 'myNameIsJenvyxu'
+
+exports.view = {
+    defaultViewEngin: 'nunjucks',
+    mapping: {
+        '.tpl': 'nunjucks'
+    }
+}
+
+exports.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  };
